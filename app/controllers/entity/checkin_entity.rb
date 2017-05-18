fe00acc7bd3abc7f7812573f585637f748c7375f
elzup <guild0105@gmail.com>
2017-05-19 02:37:47 +0900

    AcccessPoint Entity

--- app/controllers/entity/checkin_entity.rb
+++ app/controllers/entity/checkin_entity.rb
@@ -2,5 +2,6 @@ module Entity
   class CheckinEntity < Grape::Entity
     expose :id
     expose :user, using: Entity::UserEntity
+    expose :access_point, using: Entity::AccessPointEntity
   end
 end
