74ae6e1f5659db927d547bc543338bd886ae5afa
elzup <guild0105@gmail.com>
2017-05-19 02:41:03 +0900

    Checkin response

---
+++ app/controllers/v1/checkins_controller.rb
@@ -0,0 +1,47 @@
+module V1
+  class CheckinsController < Grape::API
+    helpers do
+      def current_user
+        return @current_user if @current_user
+        auth_key = request.headers['Authorization']
+        _, user_id, key = auth_key.split(':')
+        user = User.find(user_id)
+        if user.token != key
+          user = nil
+        end
+        @current_user = user
+      end
+
+      def authenticate!
+        error!('Unauthorized: 不正なユーザです。', 401) unless current_user
+      end
+    end
+
+    resource :checkins do
+      desc 'POST /checkins'
+      params do
+        requires :ssid, type: String
+        requires :bssid, type: String
+      end
+      post do
+        authenticate!
+        ap = AccessPoint.find_or_create_by({ssid: params[:ssid], bssid: params[:bssid]})
+        ap.users << current_user unless ap.users.include? current_user
+        status :created
+      end
+
+      desc 'DELETE /checkins'
+      params do
+        requires :ssid, type: String
+        requires :bssid, type: String
+      end
+      delete do
+        authenticate!
+        ap = AccessPoint.find_or_create_by({ssid: params[:ssid], bssid: params[:bssid]})
+        ap.users.delete(current_user) if ap.users.include? current_user
+        status :created
+      end
+    end
+
+  end
+end
