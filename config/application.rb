f3b1132c0a80cc66e003ba07c71b7ca7e2f2200b
elzup <guild0105@gmail.com>
2017-08-25 11:20:22 +0900

    Update log level

--- config/application.rb
+++ config/application.rb
@@ -12,6 +12,7 @@ module AsiatoroApiRails
     config.load_defaults 5.1
 
     config.logger = Logger.new(STDOUT)
+    config.logger.level = Logger::DEBUG
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
